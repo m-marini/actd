2982bc644679d2d470374e482f5ea888a958437e
marco marini <marco.marini@mmarini.org>
2016-04-15 22:36:53 +0200

    agent dump plot quantile

--- octave/plotQuantile.m
+++ octave/plotQuantile.m
@@ -1,17 +1,24 @@
-function plotQuantile(filename, Q = [0.25,0.5,0.75])
-   X = (csvread(filename)(:,2)-1/18);
+function plotQuantile(filename, Q = [0.25,0.75])
+   X = ((csvread(filename)(:,2)-1)/18);
+   #X = (csvread(filename)(:,2));
    n = size(X,1);
    m = size(Q,2);
-   Y = zeros(n, m);
+   Y = zeros(n, m + 3);
    for i = 1 : n
-     Y(i,:) = quantile(X(1:i), Q)';
+     Y(i,1:m) = quantile(X(1:i), Q)';
+     Y(i,m+1) = median(X(1:i));
+     Y(i,m+2) = mean(X(1:i));
+     Y(i,m+3) = mode(X(1:i));
    endfor
-   plot([Y movingAvg(X, n)]);
+   plot(Y);
    grid minor on;
-   L = cell(1,m+1);
+   L = cell(1,m+3);
    for i = 1 : m
     L(1,i) = sprintf("%g", Q(i));
    endfor
-   L(1,m+1)= "Mean";
+   L(1,m+1)= "Median";
+   L(1,m+2)= "Mean";
+   L(1,m+3)= "Mode";
    legend(L);
+   title("Wall Game bouncing")
 endfunction
